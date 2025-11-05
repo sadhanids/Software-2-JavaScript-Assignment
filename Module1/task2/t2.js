19308e8f44f674574da0a142615343fffb9d94a7
sadhanids <sadhanids@gmail.com>
2025-11-05 16:42:38 +0300

    Completed Task 2.

---
+++ Module1/task2/t2.js
@@ -0,0 +1,9 @@
+`user strict`;
+
+const name = prompt("Please Enter your Name")
+
+const greeting = `Hello, ${name}!!!!`
+
+console.log(greeting);
+
+document.querySelector("#target").innerHTML = greeting
